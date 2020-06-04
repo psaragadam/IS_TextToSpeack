4ee87e49fc70aa700f91525c30942f560414cf22
PK <praveen.konathala@zoomsystems.com>
2020-06-04 15:59:50 +0300

    Initial commit for text to voice

---
+++ src/main/java/com/is/textToSpeack/TextToSpeackApplication.java
@@ -0,0 +1,13 @@
+package com.is.textToSpeack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextToSpeackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextToSpeackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/is/textToSpeack/TextToSpeackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.is.textToSpeack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextToSpeackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
